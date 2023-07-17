f3a1c95d82d8c50b83dbbeb91d07bc2116ba7489
Tiago Mineiro da Silva <tiago.mineiro.silva@gmail.com>
2023-07-17 09:31:54 -0300

    Initial commit

---
+++ exercícios/test.js
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Jessica
+ */
+public class OlaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String meuPrimeiroPrograma;
+        meuPrimeiroPrograma = "Hello World!";
+        String meuSegundoPrograma;
+        meuSegundoPrograma = "Tiago";
+
+        System.out.println(meuPrimeiroPrograma);
+        
+                
+
+        // TODO code application logic here
+    }
+
+}
